462284bafcae707c8ff13791fb225558a0c6799d
Marcus Hoffmann <bubu@bubu1.eu>
2020-10-16 14:53:06 +0200

    update gradle, androidX, cleanup, ...

--- app/src/main/java/org/fdroid/fdroid/privileged/ui/UnregisterDialog.java
+++ app/src/main/java/org/fdroid/fdroid/privileged/ui/UnregisterDialog.java
@@ -5,8 +5,11 @@ import android.app.AlertDialog;
 import android.app.admin.DevicePolicyManager;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+
+import androidx.annotation.RequiresApi;
 
 import org.fdroid.fdroid.privileged.R;
 
@@ -17,8 +20,13 @@ public class UnregisterDialog extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         final String packageId = getPackageName();
+        Log.d(TAG, "onCreate: " + packageId);
+        if (Build.VERSION.SDK_INT < 21) {
+            finish();
+            return;
+        }
         final DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
-        if (dpm == null || dpm.isDeviceOwnerApp(packageId)) {
+        if (dpm == null || !dpm.isDeviceOwnerApp(packageId)) {
             finish();
             return;
         }
@@ -26,7 +34,8 @@ public class UnregisterDialog extends Activity {
                 .setTitle(getString(R.string.removal_warning_title))
                 .setIcon(R.mipmap.ic_launcher)
                 .setMessage(String.format(getString(R.string.removal_warning), getString(R.string.app_name)))
-                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
                     public void onClick(DialogInterface dialog, int whichButton) {
                         try {
                             dpm.clearDeviceOwnerApp(packageId);
@@ -37,7 +46,7 @@ public class UnregisterDialog extends Activity {
                         }
                     }
                 })
-                .setNegativeButton(android.R.string.no, null)
+                .setNegativeButton(android.R.string.cancel, null)
                 .setOnDismissListener(new DialogInterface.OnDismissListener() {
                     @Override
                     public void onDismiss(DialogInterface dialog) {
